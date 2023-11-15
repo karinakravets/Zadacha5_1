0fbc9acf710f69fb11d4e5bbf53afde2a02190c3
karina <135805670+karinakravets@users.noreply.github.com>
2023-11-15 18:43:35 +0300

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,15 +3,15 @@ class Triangle(val vertex1: Point, val vertex2: Point, val vertex3: Point)
 class Circle(val center: Point, val radius: Double)
 
 fun Triangle.getInscribedCircle(): Circle {
-    // Вычисляем длины сторон треугольника
+// Вычисляем длины сторон треугольника
     val side1 = Math.sqrt(Math.pow(vertex2.x - vertex1.x, 2.0) + Math.pow(vertex2.y - vertex1.y, 2.0))
     val side2 = Math.sqrt(Math.pow(vertex3.x - vertex2.x, 2.0) + Math.pow(vertex3.y - vertex2.y, 2.0))
     val side3 = Math.sqrt(Math.pow(vertex1.x - vertex3.x, 2.0) + Math.pow(vertex1.y - vertex3.y, 2.0))
-    // Вычисляем полупериметр треугольника
+// Вычисляем полупериметр треугольника
     val semiperimeter = (side1 + side2 + side3) / 2
-    // Вычисляем радиус окружности
+// Вычисляем радиус окружности
     val radius = Math.sqrt((semiperimeter - side1) * (semiperimeter - side2) * (semiperimeter - side3) / semiperimeter)
-    // Вычисляем координаты центра окружности
+// Вычисляем координаты центра окружности
 
     val centerX = (side2 * vertex1.x + side3 * vertex2.x + side1 * vertex3.x) / (side1 + side2 + side3)
     val centerY = (side2 * vertex1.y + side3 * vertex2.y + side1 * vertex3.y) / (side1 + side2 + side3)
@@ -38,12 +38,9 @@ fun main() {
 // Создаем объект класса "Треугольник" с заданными вершинами
     val triangle = Triangle(vertex1, vertex2, vertex3)
 
-    // Вычисляем вписанную окружность
+// Вычисляем вписанную окружность
     val inscribedCircle = triangle.getInscribedCircle()
 
     println("Координаты центра окружности: (${inscribedCircle.center.x}, ${inscribedCircle.center.y})")
     println("Радиус окружности: ${inscribedCircle.radius}")
-}2 - x1) * y) * sign
-
-    return s > 0 && t > 0 && s + t < 2 * area * sign
 }
